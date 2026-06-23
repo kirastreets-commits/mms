adc193e55a7c34904ad053ec10868b366a3253bf
kirastreets-commits <kirastreets@hotmail.com>
2026-06-23 16:49:31 +1000

    Initialize Discord bot with command setup

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+from commands import feed, heal, rest, play, start, starter, inspect, overview, explore_sanctuary, explore, journal, species, help
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
+
+
+
+@bot.event
+async def on_ready():
+    print(f"🐉 Sanctuary is online as {bot.user}")
+
+feed.setup(bot)
+heal.setup(bot)
+rest.setup(bot)
+play.setup(bot)
+starter.setup(bot)
+start.setup(bot)
+inspect.setup(bot)
+overview.setup(bot)
+explore_sanctuary.setup(bot)
+explore.setup(bot)
+journal.setup(bot)
+species.setup(bot)
+help.setup(bot)
+
+
+from systems.tutorial_cutscene import start_intro
+
+import os
+
+bot.run(os.getenv("DISCORD_TOKEN"))
