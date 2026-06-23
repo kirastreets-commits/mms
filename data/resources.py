f972062e4e0cb0cbf7c55b65c47d614af0321e0a
kirastreets-commits <kirastreets@hotmail.com>
2026-06-23 23:13:39 +1000

    Update resources.py

--- data/resources.py
+++ data/resources.py
@@ -1,14 +1,74 @@
-resources = [
-  Sweetberries,
-  Healing Moss,
-  Moonflowers,
-  Warm Pebbles,
-  Firebloom Petals,
-  Fluffy Tufts,
-  Wildflowers,
-  Sweet Grass,
-  Polished Stones,
-  Shimmering Feathers,
-  Strange Trinket,
-  Golden Coin
-  ]
+RESOURCES = {
+    "sweetberries": {
+        "name": "Sweetberries",
+        "emoji": "🍓",
+        "rarity": "common",
+        "type": "food"
+    },
+    "healing_moss": {
+        "name": "Healing Moss",
+        "emoji": "🌿",
+        "rarity": "common",
+        "type": "medicine"
+    },
+    "moonflowers": {
+        "name": "Moonflowers",
+        "emoji": "🌙",
+        "rarity": "rare",
+        "type": "special"
+    },
+    "warm_pebbles": {
+        "name": "Warm Pebbles",
+        "emoji": "🪨",
+        "rarity": "common",
+        "type": "nest"
+    },
+    "firebloom_petals": {
+        "name": "Firebloom Petals",
+        "emoji": "🌺",
+        "rarity": "uncommon",
+        "type": "nest"
+    },
+    "fluffy_tufts": {
+        "name": "Fluffy Tufts",
+        "emoji": "☁️",
+        "rarity": "common",
+        "type": "nest"
+    },
+    "wildflowers": {
+        "name": "Wildflowers",
+        "emoji": "🌸",
+        "rarity": "common",
+        "type": "nest"
+    },
+    "sweet_grass": {
+        "name": "Sweet Grass",
+        "emoji": "🌾",
+        "rarity": "common",
+        "type": "nest"
+    },
+    "polished_stones": {
+        "name": "Polished Stones",
+        "emoji": "💎",
+        "rarity": "uncommon",
+        "type": "nest"
+    },
+    "shimmering_feathers": {
+        "name": "Shimmering Feathers",
+        "emoji": "🪶",
+        "rarity": "uncommon",
+        "type": "nest"
+    },
+    "strange_trinket": {
+        "name": "Strange Trinket",
+        "emoji": "✨",
+        "rarity": "rare",
+        "type": "collectible"
+    },
+    "golden_coin": {
+        "name": "Golden Coin",
+        "emoji": "🪙",
+        "rarity": "rare",
+        "type": "collectible"
+    }
+}
