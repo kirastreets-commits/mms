6100980bae791a731b85d2c351cf99ecf21e3d03
kirastreets-commits <kirastreets@hotmail.com>
2026-06-24 22:39:09 +1000

    Update creature.py

--- models/creature.py
+++ models/creature.py
@@ -17,6 +17,7 @@ class Creature:
         name: str,
         species: str,
         personality=None,
+        shelter=None
         mood="neutral",
         trust: int = 20,
         health: int = 20,
