ababe74cb5a2999c1436bb92b1cba6150671d710
kirastreets-commits <kirastreets@hotmail.com>
2026-06-25 06:32:40 +1000

    Update start.py

--- commands/start.py
+++ commands/start.py
@@ -3,10 +3,10 @@ from systems.tutorial_cutscene import start_intro
 
 
 def setup(bot):
+    print("✅ start.py loaded")
 
     @bot.command()
     async def start(ctx):
-
+        print("🔥 start command triggered")
         player = get_or_create_player(ctx.author)
-
-        await start_intro(ctx, player)
+        await start_intro(ctx, play
