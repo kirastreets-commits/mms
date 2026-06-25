a0e1b86bd7e4e95713b7cc9f72e952fa3edd0a14
kirastreets-commits <kirastreets@hotmail.com>
2026-06-26 00:10:29 +1000

    Update embed_builder.py

--- systems/embed_builder.py
+++ systems/embed_builder.py
@@ -1,5 +1,5 @@
 import discord
-from systems.gift_responses import get_gift_message
+from data.gift_responses import get_gift_message
 
 def build_gift_embed(creature, result):
     embed = discord.Embed(
