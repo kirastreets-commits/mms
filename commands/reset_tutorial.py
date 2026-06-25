bbacc61dd14b56322538182b2a967f4dff4000fa
kirastreets-commits <kirastreets@hotmail.com>
2026-06-25 11:51:45 +1000

    Refactor reset_tutorial command setup

---
+++ commands/reset_tutorial.py
@@ -0,0 +1,13 @@
+def setup(bot)
+
+    @bot.command()
+    async def reset_tutorial(ctx):
+
+        player = get_or_create_player(ctx.author)
+
+        player.tutorial_stage = 0
+        player.tutorial_complete = False
+
+        save_player(player)
+
+        await ctx.send("Tutorial reset.")
