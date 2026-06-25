bd179705167193818d30b1698e3a47a554b14051
kirastreets-commits <kirastreets@hotmail.com>
2026-06-25 23:44:54 +1000

    Fix indentation in gift command function

--- commands/gift.py
+++ commands/gift.py
@@ -4,18 +4,18 @@ from systems.gift_system import gift_creature
 def setup(bot):
 
     @bot.command()
-async def gift(ctx, creature_name: str, item_id: str):
-    player = get_or_create_player(ctx.author)
-    creature = player.get_creature(creature_name)
-
-    if not creature:
-        return await ctx.send("You don't have that creature.")
-
-    result = gift_creature(player, creature, item_id)
-
-    if not result["success"]:
-        return await ctx.send(result["message"])
-
-    embed = build_gift_embed(creature, item_id, result)
-
-    await ctx.send(embed=embed)
+    async def gift(ctx, creature_name: str, item_id: str):
+        player = get_or_create_player(ctx.author)
+        creature = player.get_creature(creature_name)
+    
+        if not creature:
+            return await ctx.send("You don't have that creature.")
+    
+        result = gift_creature(player, creature, item_id)
+    
+        if not result["success"]:
+            return await ctx.send(result["message"])
+    
+        embed = build_gift_embed(creature, item_id, result)
+    
+        await ctx.send(embed=embed)
