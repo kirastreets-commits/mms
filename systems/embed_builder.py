3261bc45ddc64a3b5312593080312bb1bcbe9d2d
kirastreets-commits <kirastreets@hotmail.com>
2026-06-25 23:57:09 +1000

    Create embed_builder.py

---
+++ systems/embed_builder.py
@@ -0,0 +1,18 @@
+def build_gift_embed(creature, result):
+    embed = discord.Embed()
+
+    embed.description = get_gift_message(creature, result)
+
+    embed.add_field(
+        name="Bond",
+        value=f"+{result['bond_gain']}",
+        inline=True
+    )
+
+    embed.add_field(
+        name="Comfort",
+        value=f"+{result['comfort_gain']}",
+        inline=True
+    )
+
+    return embed
