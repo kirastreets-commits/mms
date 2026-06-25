69e9f29a9b50f001c7071b350dea03ec042b2992
kirastreets-commits <kirastreets@hotmail.com>
2026-06-25 20:32:25 +1000

    Add files via upload

---
+++ commands/gift.py
@@ -0,0 +1,19 @@
+def setup(bot):
+
+    @bot.command()
+    async def gift(ctx, creature_name: str, item_id: str):
+        player = get_or_create_player(ctx.author)
+        creature = player.get_creature(creature_name)
+
+        if not creature:
+            return await ctx.send("You don't have that creature.")
+
+        result = gift_creature(player, creature, item_id)
+
+        if not result["success"]:
+            return await ctx.send(result["message"])
+
+        await ctx.send(
+            f"{creature.name} {result['reaction']} the gift!\n"
+            f"+{result['bond_gain']} bond"
+    )
