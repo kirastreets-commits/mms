8451124002459ed41dfc3f4a4dc7c97bdfbd1d9c
kirastreets-commits <kirastreets@hotmail.com>
2026-06-25 11:53:04 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-from commands import feed, heal, rest, play, start, starter, inspect, overview, explore_sanctuary, explore, journal, species, help
+from commands import feed, heal, rest, play, start, starter, inspect, overview, explore_sanctuary, explore, journal, species, reset_tutorial, help
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -37,7 +37,7 @@ explore.setup(bot)
 journal.setup(bot)
 species.setup(bot)
 help.setup(bot)
-
+reset_tutorial.setup(bot)
 
 from systems.tutorial_cutscene import start_intro
 
