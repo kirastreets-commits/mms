33fb7895cac1741bc12e8c7787faaf12f25256ce
kirastreets-commits <kirastreets@hotmail.com>
2026-06-26 19:39:57 +1000

    Add CreatureSelect class for creature selection

--- ui/components/creature_select.py
+++ ui/components/creature_select.py
@@ -19,3 +19,20 @@ class CreatureSelectView(discord.ui.View):
         ]
 
         self.add_item(CreatureSelect(options))
+
+class CreatureSelect(discord.ui.Select):
+    def __init__(self, options):
+        super().__init__(
+            placeholder="Choose a creature...",
+            min_values=1,
+            max_values=1,
+            options=options
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        view: CreatureSelectView = self.view
+
+        creature_name = self.values[0]
+
+        # call the NPC-provided callback
+        await view.on_select_callback(interaction, creature_name)
