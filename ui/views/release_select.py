6f1e9f0772d64ff8022e3d36611ce1df727c0809
kirastreets-commits <kirastreets@hotmail.com>
2026-06-27 23:30:25 +1000

    Update release_select.py

--- ui/views/release_select.py
+++ ui/views/release_select.py
@@ -5,13 +5,26 @@ class ReleaseSelectView(discord.ui.View):
 
         player = get_or_create_player_by_id(user_id)
 
-        options = [
-            discord.SelectOption(
-                label=creature.nickname or creature.name,
-                value=creature.name
-            )
+        options = []
+
+        # Remove any corrupted creatures
+        player.creatures = [
+            creature
             for creature in player.creatures
+            if hasattr(creature, "name") and isinstance(creature.name, str)
         ]
+        
+        save_player(player)
+        
+        for creature in player.creatures:
+            options.append(
+                discord.SelectOption(
+                    label=creature.name,
+                    value=creature.name
+                )
+            )
+
+self.add_item(ReleaseSelect(options))
 
         self.add_item(ReleaseSelect(options))
 
