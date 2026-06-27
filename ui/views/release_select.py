c38c2b803cd0b03ec57e2e153c95d82c19499818
kirastreets-commits <kirastreets@hotmail.com>
2026-06-27 23:09:11 +1000

    Update release_select.py

--- ui/views/release_select.py
+++ ui/views/release_select.py
@@ -1,14 +1,29 @@
-class ReleaseSelectView(discord.ui.View):
-    def __init__(self, user_id):
-        super().__init__(timeout=120)
-        self.user_id = user_id
+class ReleaseSelect(discord.ui.Select):
+    def __init__(self, options):
+        super().__init__(
+            placeholder="Choose a creature to release...",
+            min_values=1,
+            max_values=1,
+            options=options
+        )
 
-    @discord.ui.select(placeholder="Choose a creature to release")
-    async def select_creature(self, interaction: discord.Interaction, select: discord.ui.Select):
+    async def callback(self, interaction: discord.Interaction):
 
-        creature_name = select.values[0]
+        creature_name = self.values[0]
+
+        player = get_or_create_player(interaction.user)
+        creature = player.get_creature(creature_name)
+
+        if not creature:
+            return await interaction.response.send_message(
+                "That creature could not be found.",
+                ephemeral=True
+            )
+
+        player.creatures.remove(creature)
+        save_player(player)
 
         await interaction.response.send_message(
-            f"Are you sure you want to release **{creature_name}**?",
+            f"🕊️ {creature.nickname or creature.name} has been gently released back into the wild.",
             ephemeral=True
         )
