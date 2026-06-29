084c9863748022e4727c54c3a1961e1ce402db89
kirastreets-commits <kirastreets@hotmail.com>
2026-06-29 16:40:58 +1000

    Update explore_views.py

--- ui/explore_views.py
+++ ui/explore_views.py
@@ -6,7 +6,7 @@ from data.locations import LOCATIONS
 from data.resources import RESOURCES
 from data.species import get_species
 from models.creature import Creature
-from ui.modals.rename_modal import RenameModal
+from ui.modals.adopt_name_modal import AdoptNameModal
 
 class ExploreMenuView(discord.ui.View):
     def __init__(self, player):
@@ -376,7 +376,7 @@ class NameCreatureView(discord.ui.View):
     ):
 
         await interaction.response.send_modal(
-            RenameModal(self.player, self.species_name)
+            AdoptNameModal(self.player, self.species_name)
         )
 
     @discord.ui.button(
