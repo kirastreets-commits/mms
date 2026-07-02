24d6feee28ffac45050e062609a1c8651ca3247f
kirastreets-commits <kirastreets@hotmail.com>
2026-07-02 18:51:30 +1000

    Update gift_views.py

--- ui/gift_views.py
+++ ui/gift_views.py
@@ -6,7 +6,6 @@ from systems.action_renderer import render_action_embed
 from systems.gift_system import apply_gift_outcome, build_gift_embed, gift_creature, give_item
 from systems.memory_system import update_memory
 from systems.save_system import save_player
-from models.player import remove_from_inventory
 
 class GiftResourceSelect(discord.ui.Select):
 
@@ -52,24 +51,7 @@ class GiftResourceSelect(discord.ui.Select):
         self.player = player
         gift_item = None
 
-        if self.values[0] != "none":
-            item_id = self.values[0]
-            gift_item = RESOURCES[item_id]
-            self.player.remove_from_inventory(item_id, 1)
-
-        # reaction
-        result = give_item(creature, item_id, player.user_id)
-
-        # apply world changes
-        apply_gift_outcome(creature, item_id, result)
-
-        # memory system
-        update_memory(creature, "gift", result)
-
-        return {
-            "success": True,
-            **result
-        }
+        result = gift_creature(player, creature, item_id)
 
         save_player(self.player)
 
