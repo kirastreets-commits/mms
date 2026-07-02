ffcb98ebf5a9c4d61eaed2ead57c992124e30b31
kirastreets-commits <kirastreets@hotmail.com>
2026-07-02 18:19:35 +1000

    Update gift.py

--- commands/gift.py
+++ commands/gift.py
@@ -1,24 +1,23 @@
 from systems.save_system import get_or_create_player, save_player
 from systems.gift_system import gift_creature
-from systems.embed_builder import build_gift_embed
+from ui.gift_views import GiftView
 
 def setup(bot):
 
     @bot.command()
-    async def gift(ctx, creature_name: str, item_id: str):
+    async def gift(ctx, creature_name: str):
         player = get_or_create_player(ctx.author)
         creature = player.get_creature(creature_name)
     
         if not creature:
             return await ctx.send("You don't have that creature.")
-    
-        result = gift_creature(player, creature, item_id)
-    
-        if not result["success"]:
-            return await ctx.send(result["message"])
 
         save_player(player)
     
-        embed = build_gift_embed(creature, result)
-    
-        await ctx.send(embed=embed)
+        view = GiftView(player, creature)
+        
+        await ctx.send(
+                f"Choose a gift for **{creature.name}**.",
+                view=view
+            )
+
